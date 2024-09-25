84b62718eb2b056d9486ebe173dfa4984dc74428
Amanda Dias <168840059+mnddevl@users.noreply.github.com>
2024-09-25 10:03:46 +0000

    texto editado

--- src/front/js/pages/quienSomos.js
+++ src/front/js/pages/quienSomos.js
@@ -10,7 +10,7 @@ function QuienSomos() {
                     </div>
                     <div className="col-md-6 text-container">
                         <h2>Cómo empezó todo</h2>
-                        <p>En 2024, cuatro amigos amantes del café decidieron fundar una empresa dedicada a ofrecer café de calidad para llevar...</p>
+                        <p>En 2024, amigos amantes del café decidieron fundar una empresa dedicada a ofrecer café de calidad para llevar. Inspirados por la filosofía de "menos es más", se enfocaron en perfeccionar cada aspecto del proceso, desde la cosecha hasta la taza. Cada uno aportaba su experiencia en cata, tueste y mejora de la experiencia del café. Para ellos, beber café es un ritual que aporta calma y felicidad, y su objetivo es compartir esa transformación diaria con la mayor cantidad de personas posible, tanto en sus cafeterías como en los hogares de sus clientes.</p>
                     </div>
                 </div>
             </div>
@@ -19,7 +19,7 @@ function QuienSomos() {
                 <div className="row g-0">
                     <div className="col-md-6 text-container">
                         <h2>Buen Café, para todos los paladares</h2>
-                        <p>La visión de los cuatro amigos era hacer que el buen café fuera accesible para todos...</p>
+                        <p>La visión de esos amigos era hacer que el buen café fuera accesible para todos. Comenzaron colaborando con productores en las fincas para asegurar la calidad de cada grano. Se enfocan en cada etapa del proceso, desde la cata hasta el tueste, seleccionando solo los granos excepcionales y priorizando la calidad sobre la cantidad. Creen que el café de especialidad no es un lujo, sino una alternativa al café comercial, y están comprometidos a ofrecer una experiencia de calidad para todos, sin importar el paladar.</p>
                     </div>
                     <div className="col-md-6 image-container">
                         <img src="cafe-negocio.webp" alt="Buen Café" />
@@ -34,7 +34,7 @@ function QuienSomos() {
                     </div>
                     <div className="col-md-6 text-container">
                         <h2>Café de especialidad, sostenible para los productores y el medio ambiente</h2>
-                        <p>Desde el principio, los cuatro amigos compartieron la visión de ofrecer café de especialidad...</p>
+                        <p>Desde el principio, los amigos compartieron la visión de ofrecer café de especialidad que beneficie tanto a los consumidores como a los productores y al planeta. Trabajan exclusivamente con pequeñas fincas, gestionadas por personas y familias a las que conocen, basándose en relaciones de comercio directo y justo. Esto permite a los productores mejorar sus tierras y fortalecer sus comunidades. Su café es 100% arábica, de origen único y cultivado de manera sostenible. Su misión va más allá de ofrecer un café excepcional; buscan generar un impacto positivo en las comunidades productoras y en el medio ambiente, viendo cada taza como una oportunidad para crear un cambio significativo.</p>
                     </div>
                 </div>
             </div>
